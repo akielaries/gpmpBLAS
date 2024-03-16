b273e387dcbfc3f09e1ca02d37e45244b8c8daaa
akielaries <akiel@akiel.org>
2024-03-15 20:41:29 -0700

    updating DGEMM CPP interface

--- gpmpBLAS.hpp
+++ gpmpBLAS.hpp
@@ -5,7 +5,40 @@
 #ifndef GPMPBLAS_HPP
 #define GPMPBLAS_HPP
 
+#include <stdint.h>
 
-void dgemm();
+extern "C" {
+    void DGEMM_(char        *TRANSA, 
+                char        *TRANSB, 
+                uint32_t    M, 
+                uint32_t    N, 
+                uint32_t    K, 
+                double      ALPHA, 
+                double      *A, 
+                uint32_t    LDA, 
+                double      *B, 
+                uint32_t    LDB, 
+                double      BETA, 
+                double      *C, 
+                uint32_t    LDC);
+}
+
+void dgemm(char        *TRANSA, 
+           char        *TRANSB, 
+           uint32_t    M,  
+           uint32_t    N,  
+           uint32_t    K,  
+           double      ALPHA, 
+           double      *A, 
+           uint32_t    LDA, 
+           double      *B, 
+           uint32_t    LDB, 
+           double      BETA, 
+           double      *C, 
+           uint32_t    LDC){
+
+    DGEMM_(TRANSA, TRANSB, M, N, K, ALPHA, A, LDA, B, LDB, BETA, C, LDC);
+
+}
 
 #endif
